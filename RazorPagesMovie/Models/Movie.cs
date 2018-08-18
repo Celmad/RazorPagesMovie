8c218809028bf9b9451b16d45b420aad3f3cfbe0
manuel alaminos dominguez <tandesobedientecomoelvientodeponiente@hotmail.com>
2018-08-18 14:09:54 +0100

    ReleaseDate now is being displayed as "Release Date". Price is now correctly mapped in the database as currency. Changed Create, Details and Delete directory names with id.

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -6,8 +9,13 @@ namespace RazorPagesMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
